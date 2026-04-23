4bc448d7938cff0312a23dfe83ef691400407ce7
kaushik3pg <piyush.kaushik@3pillarglobal.com>
2026-04-23 16:42:45 +0530

    refactoring compelte

--- src/components/DeleteBudgetAction.tsx
+++ src/components/DeleteBudgetAction.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Trash2 } from "lucide-react";
 import { ConfirmActionButton } from "./ConfirmActionButton";
 import { deleteBudget } from "@/app/db/actions";
 
@@ -12,11 +11,8 @@ export function DeleteBudgetAction({ budgetId }: { budgetId: string }) {
   return (
     <ConfirmActionButton
       onConfirm={handleDelete}
-      triggerElement={
-        <button className="p-2 text-muted-foreground hover:text-red-600 hover:bg-red-50 rounded-md transition-colors">
-          <Trash2 className="w-5 h-5" />
-        </button>
-      }
+      variant={"danger-button"}
+      label="Delete"
       title="Delete this Budget?"
       description="This action is irreversible. All transactions will be lost."
     />
